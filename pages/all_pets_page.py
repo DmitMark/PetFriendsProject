091a9296a23e4a5218dd73ba7e782ff253c4b509
DmitMark <dmmarkushevski@gmail.com>
2023-09-15 11:28:18 +0300

    Initial commit

---
+++ pages/all_pets_page.py
@@ -0,0 +1,30 @@
+import os,pickle
+
+from pages.base import WebPage
+from pages.elements import WebElement
+from pages.elements import ManyWebElements
+
+
+
+class AllPetsPage(WebPage):
+
+    def __init__(self, web_driver, url = ''):
+        url = 'https://petfriends.skillfactory.ru/all_pets'
+        super().__init__(web_driver, url)
+        with open('my_cookies.txt', 'rb') as cookiesfile:
+            cookies = pickle.load(cookiesfile)
+            for cookie in cookies:
+                web_driver.add_cookie(cookie)
+            web_driver.refresh()
+
+
+
+    btn_my_pets = WebElement(XPATH='//*[@id="navbarNav"]/ul[1]/li[1]/a[1]')
+    #
+    # password = WebElement(id='pass')
+    #
+    # btn = WebElement(class_name='btn.btn-success')
+
+
+
+
---
+++ tests/test_all_pets_page.py
@@ -0,0 +1,11 @@
+# python -m pytest -v --driver Chrome --driver-path C:\chromedriver.exe tests/test_all_pets_page.py
+
+import pytest
+import time, pickle
+from pages.all_pets_page import AllPetsPage
+from selenium.webdriver.common.action_chains import ActionChains
+
+def test_get_all_pets_page(web_browser):
+    page = AllPetsPage(web_browser)
+
+    assert page.get_current_url() == 'https://petfriends.skillfactory.ru/all_pets'
