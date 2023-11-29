0c30e98aff4da5c884975f2aa808d2d0a37a71a3
DmitMark <dmmarkushevski@gmail.com>
2023-11-29 15:39:40 +0300

    refactoring api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,34 +1,37 @@
+# python -m pytest tests/test_api.py
+import json.decoder
+
 from api.api import PetFriendsApi
 from settings import valid_email, valid_password, invalid_email, invalid_password
 
 pf = PetFriendsApi()
-def test_get_api_key_for_valid_user(email = valid_email, password = valid_password):
+def test_get_api_key_for_valid_user(email=valid_email,password=valid_password):
     """ Проверяем что запрос api ключа возвращает статус 200 и в результате содержится слово key"""
 
-    status, result = pf.get_api_key(email, password)
-
-    assert status == 200
-    assert 'key' in result
+    assert pf.get_api_key(email, password).status_code == 200
+    assert 'key' in pf.get_api_key(email, password).json()
 
 
-def test_get_api_key_for_invalid_user(email = invalid_email, password = invalid_password):
+def test_get_api_key_for_invalid_user(email=invalid_email, password=invalid_password):
     """ Проверяем что запрос api ключа возвращает статус 403 при использовании данных
-    незарегистрированного пользователя(отсутствует в базе) и в ответе нет слова key"""
+    незарегистрированного пользователя(отсутствует в базе)"""
 
-    status, result = pf.get_api_key(email, password)
-
-    assert status == 403
-    assert 'This user wasn&#x27;t found in database' in result
+    assert pf.get_api_key(email,password).status_code == 403
+    try:
+        assert 'This user wasn&#x27;t found in database' in pf.get_api_key(email,password).json()
+    except json.decoder.JSONDecodeError:
+        assert 'This user wasn&#x27;t found in database' in pf.get_api_key(email, password).text
 
 
 def test_get_api_key_for_valid_user_invalid_password(email=valid_email, password=invalid_password):
     """ Проверяем что запрос api ключа возвращает статус 403 при использовании неверного пароля
-     пользователя(отсутствует в базе) и в ответе нет слова key"""
+     пользователя(отсутствует в базе)"""
 
-    status, result = pf.get_api_key(email, password)
-
-    assert status == 403
-    assert 'This user wasn&#x27;t found in database' in result
+    assert pf.get_api_key(email,password).status_code == 403
+    try:
+        assert 'This user wasn&#x27;t found in database' in pf.get_api_key(email, password).json()
+    except json.decoder.JSONDecodeError:
+        assert 'This user wasn&#x27;t found in database' in pf.get_api_key(email, password).text
 
 
 def test_get_all_pets_with_valid_key(filter=''):
@@ -36,8 +39,7 @@ def test_get_all_pets_with_valid_key(filter=''):
         Для этого сначала получаем api ключ и сохраняем в переменную auth_key. Далее используя этого ключ
         запрашиваем список всех питомцев и проверяем что список не пустой."""
 
-    _, auth_key = pf.get_api_key(valid_email, valid_password)
-    status, result = pf.get_list_of_pets(auth_key, filter)
+    auth_key = pf.get_api_key(valid_email, valid_password).json()
 
-    assert status == 200
-    assert len(result['pets']) > 0
+    assert pf.get_list_of_pets(auth_key, filter).status_code == 200
+    assert len(pf.get_list_of_pets(auth_key, filter).json()['pets']) > 0
